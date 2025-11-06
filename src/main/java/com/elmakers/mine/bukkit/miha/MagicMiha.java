ce394761712ab5872032bf1844460c9800b92c34
Nathan Wolf <nathan@elmakers.com>
2025-11-06 14:44:36 -0800

    Implement help command

--- src/main/java/com/elmakers/mine/bukkit/miha/MagicMiha.java
+++ src/main/java/com/elmakers/mine/bukkit/miha/MagicMiha.java
@@ -2,6 +2,8 @@ package com.elmakers.mine.bukkit.miha;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import org.bukkit.Bukkit;
@@ -17,7 +19,12 @@ import com.elmakers.mine.bukkit.miha.dummy.DummyServer;
 import com.elmakers.mine.bukkit.miha.dummy.SystemCommandSender;
 import com.elmakers.mine.bukkit.miha.evaluate.EvaluateTask;
 import com.elmakers.mine.bukkit.miha.platform.Platform;
+import com.elmakers.mine.bukkit.utility.ChatUtils;
 import com.elmakers.mine.bukkit.utility.CompatibilityLib;
+import com.elmakers.mine.bukkit.utility.StringUtils;
+import com.elmakers.mine.bukkit.utility.help.Help;
+import com.elmakers.mine.bukkit.utility.help.HelpTopic;
+import com.elmakers.mine.bukkit.utility.help.HelpTopicMatch;
 
 public class MagicMiha {
     private final MagicController controller;
@@ -54,6 +61,10 @@ public class MagicMiha {
             while (running) {
                 System.out.print("> ");
                 String command = scanner.nextLine();
+                String[] commandAndArgs = command.split(" ");
+                command = commandAndArgs[0];
+                String[] args = new String[commandAndArgs.length - 1];
+                System.arraycopy(commandAndArgs, 1, args, 0, args.length);
 
                 switch (command.toLowerCase()) {
                     case "stop":
@@ -61,7 +72,20 @@ public class MagicMiha {
                         running = false;
                         break;
                     case "evaluate":
-                        evaluate();
+                        int repeat = 0;
+                        if (args.length > 0) {
+                            try {
+                                repeat = Integer.parseInt(args[0]);
+                            } catch (Exception ex) {
+                                sender.sendMessage("Invalid count: " + args[0]);
+                                return;
+                            }
+                        }
+                        evaluate(repeat);
+                        break;
+                    case "help":
+                    case "mhelp":
+                        help(args);
                         break;
                     default:
                         sender.sendMessage("Unknown command: " + command);
@@ -73,12 +97,31 @@ public class MagicMiha {
         }
     }
 
-    private void evaluate() {
+    private void help(String[] args) {
+        Help help = controller.getMessages().getHelp();
+        if (args.length == 1) {
+            HelpTopic topic = help.getTopic(args[0]);
+            if (topic != null) {
+                sender.sendMessage(topic.getText());
+                return;
+            }
+        }
+
+        List<String> keywords = Arrays.asList(ChatUtils.getWords(StringUtils.join(args, " ").toLowerCase()));
+        List<HelpTopicMatch> matches = help.findMatches(keywords, 1);
+        if (matches.isEmpty()) {
+            sender.sendMessage( "No matches found");
+        } else {
+            sender.sendMessage(matches.get(0).getTopic().getText());
+        }
+    }
+
+    private void evaluate(int repeat) {
         YamlConfiguration goals = new YamlConfiguration();
         Plugin plugin = controller.getPlugin();
         try {
             goals.load(new InputStreamReader(plugin.getResource("goals.yml")));
-            EvaluateTask evaluateTask = new EvaluateTask(sender, plugin, controller, goals, 1);
+            EvaluateTask evaluateTask = new EvaluateTask(sender, plugin, controller, goals, repeat);
             Thread evaluateThread = new Thread(evaluateTask);
             evaluateThread.start();
         } catch (Exception ex) {
